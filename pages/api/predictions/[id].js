2f3aab9396bcd2ab16bcc5ea256e94d55dc40107
Sayak Maity <smaity@college.harvard.edu>
2022-10-14 14:33:30 -0400

    commit1

---
+++ pages/api/predictions/[id].js
@@ -0,0 +1,20 @@
+export default async function handler(req, res) {
+  const response = await fetch(
+    "https://api.replicate.com/v1/predictions/" + req.query.id,
+    {
+      headers: {
+        Authorization: `Token eeda156dcec93261cd345fe728a920d15a804757`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  if (response.status !== 200) {
+    let error = await response.json();
+    res.statusCode = 500;
+    res.end(JSON.stringify({ detail: error.detail }));
+    return;
+  }
+
+  const prediction = await response.json();
+  res.end(JSON.stringify(prediction));
+}
